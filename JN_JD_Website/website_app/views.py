0fabe8bd61a4680d2089250fe42dd1a0f53863ac
Josh Nelson <joshuaknelson1202@icloud.com>
2025-02-06 10:58:41 -0700

    created sign in functionaility

--- JN_JD_Website/website_app/views.py
+++ JN_JD_Website/website_app/views.py
@@ -1,40 +1,42 @@
 from django.contrib.auth import authenticate
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login as auth_login
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from .forms import SignInForm
 
-def index(request):
-    context = {
-
-    }
+@login_required
+def home(request):
+    # You can populate the context with data specific to the user or the page
+    context = {}
     return render(request, 'home.html', context)
 
 def about(request):
-    context = {
-        
-    }
+    # About page context, you can update it if necessary
+    context = {}
     return render(request, 'index.html', context)
 
-def login(request):
-    context = {
-        
-    }
-    return render(request, 'login.html', context)
+def create_account(request):
+    # This is the login page view
+    context = {}
+    return render(request, 'createaccount.html', context)
 
 def sign_in(request):
     if request.method == "POST":
-        form = SignInForm(request, data=request.POST)
+        form = SignInForm(data=request.POST)
         if form.is_valid():
             user = form.get_user()
             auth_login(request, user)
 
+            # Handle the remember me functionality
             if not form.cleaned_data.get('remember_me'):
                 request.session.set_expiry(0)
-            return redirect('about')
+            return redirect('home')
     else:
         form = SignInForm()
+
+    # Context for the signin page to display the form
     context = {
-        'form':form
+        'form': form
     }
     return render(request, 'signin.html', context)
